a969bb289975d60d176f7dd931f50d510e519fa4
Karim Ali QC Engineer <karimaliahmed9@gmail.com>
2026-09-09 14:11:15 +0300

    Updated the project

--- src/RevitionBasics/revitiontojava.java
+++ src/RevitionBasics/revitiontojava.java
@@ -1,22 +1,23 @@
 package RevitionBasics;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class revitiontojava {
     public static void main(String[] args) {
 
-      /*  int nul1 = 4;
+        int nul1 = 4;
         double null2 = 650.25;
         double null3 = nul1 * 0.5 * null2;
-        System.out.println("null 3 Equall :"+ null3 + "\n I am a Good Tester $$$");*/
+        System.out.println("null 3 Equall :" + null3 + "\n I am a Good Tester $$$");
 
-        /*System.out.println("Please Enter Your Weight In Pound");
+        System.out.println("Please Enter Your Weight In Pound");
         Scanner input = new Scanner(System.in);
         double weightinpound = input.nextDouble();
         double weightinKg = weightinpound * 0.454;
-        System.out.println("Your Weight In Pound : " + weightinpound + "= Your Weight In Kg :"+ weightinKg);*/
+        System.out.println("Your Weight In Pound : " + weightinpound + "= Your Weight In Kg :" + weightinKg);
 
-       /* Scanner input = new Scanner(System.in);
+        //Scanner input = new Scanner(System.in);
         System.out.println("Please Enter Hours");
         double hours = input.nextDouble();
 
@@ -27,26 +28,26 @@ public class revitiontojava {
         double sec = input.nextDouble();
 
         double timeinsecond = (hours * 60 * 60) + (min * 60) + sec;
-        System.out.println("Your Time In Second " + "\n "+timeinsecond);*/
+        System.out.println("Your Time In Second " + "\n " + timeinsecond);
 
-        /*final int Transportation = 600;
+        final int Transportation = 600;
         System.out.println("Please Enter Your Basic Salary ?");
-        Scanner input = new Scanner(System.in);
+        //Scanner input = new Scanner(System.in);
         double basicsalary = input.nextDouble();
         double allowance = 0;
         if (basicsalary >= 15000)
             allowance = basicsalary * 0.05;
 
         else if (basicsalary >= 10000 && basicsalary <= 14999)
-            allowance =  0.10;
-        else if (basicsalary >= 5000 && basicsalary <=9999)
-            allowance =  0.15;
+            allowance = 0.10;
+        else if (basicsalary >= 5000 && basicsalary <= 9999)
+            allowance = 0.15;
         else allowance = 0.20;
 
         double TotalSalary = Transportation + basicsalary + (basicsalary * allowance);
-        System.out.println("The Total Salary :" + TotalSalary);*/
+        System.out.println("The Total Salary :" + TotalSalary);
 
-      /* ArrayList<String> list = new ArrayList<String>();
+        ArrayList<String> list = new ArrayList<String>();
         list.add("Ahmed");
         list.add("Karim");
         list.add("Mohamed");
@@ -54,11 +55,10 @@ public class revitiontojava {
         System.out.println(list.get(1));
         System.out.println(list.add("Yousef"));
         System.out.println(list);
-        list.add(2,"saad");
-        System.out.println(list);*/
+        list.add(2, "saad");
+        System.out.println(list);
 
     }
-
 
 
 }
